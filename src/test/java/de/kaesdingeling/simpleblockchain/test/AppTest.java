a00a22a1beb158df5cc138e52b8a8a81874b735a
KaesDingeling <kingdingeling721@gmail.com>
2019-07-20 18:53:02 +0200

    update test

--- src/test/java/de/kaesdingeling/simpleblockchain/test/AppTest.java
+++ src/test/java/de/kaesdingeling/simpleblockchain/test/AppTest.java
@@ -22,7 +22,6 @@ public class AppTest {
 						.minNonces(0)
 						.maxNonces(500000)
 						.thread(1)
-						.stopByResult(false)
 						.controllerPreLoadDuration(2)
 						.controllerUpdateDuration(5)
 						.build())
@@ -44,11 +43,19 @@ public class AppTest {
 	}
 	
 	private boolean isMatchAResultInSimpleTest(ResultData result) {
-		if (result.getHash().equals("0000faf8cd931548f22db71dacafcc3044f1450097ff1d6d6f3aeef8ef283d35") && result.getNonce() == 370091) {
+		if (result.getHash().equals("00008a395ab9549f049b50db309c4fafa6369669a840de16ac7c72bcda00adb1") && result.getNonce() == 379716) {
 			return true;
 		}
 
-		if (result.getHash().equals("0000c8f2f0ed74d010ab25f6c836d75274e2780449469caf069fc00f92c6ab3b") && result.getNonce() == 67494) {
+		if (result.getHash().equals("00007c350839c019471ad05629fc763bc00d5f6d04a8820f5743f359b2540543") && result.getNonce() == 277542) {
+			return true;
+		}
+
+		if (result.getHash().equals("000087f72f9e41f9cfe9ddb46e34b210c4227e134301c6eb63a5f1d402e04c5e") && result.getNonce() == 73830) {
+			return true;
+		}
+
+		if (result.getHash().equals("0000e639831ae8244e7279016c6747ce2b74dc484b36bbc322c92e8e3c674602") && result.getNonce() == 223705) {
 			return true;
 		}
 		
